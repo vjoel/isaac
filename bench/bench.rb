9729ad198f466b7725369fd6fe3727ec8e381ee8
Joel VanderWerf <vjoel@users.sourceforge.net>
2013-04-19 23:02:34 -0700

    fix include path and update bench results

--- bench/bench.rb
+++ bench/bench.rb
@@ -1,5 +1,5 @@
 require 'benchmark'
-require '../ext/isaac/isaac'
+require 'isaac'
 
 srand(1) # use ruby's prng to seed isaac
 ary = (0..255).map {rand(2**32-1)}
@@ -19,8 +19,10 @@ __END__
 Times to generate 1_000_000 numbers:
 
 With 256 entry state vectors (crypto):
-  1.860000   0.200000   2.060000 (  2.185345)
+  1.860000   0.200000   2.060000 (  2.185345) # 2005 ThinkPad
+  0.780000   0.000000   0.780000 (  0.782771) # 2008 Eee901
   
 With 16 entry state vectors (ok for simulation):
-  1.900000   0.200000   2.100000 (  2.224832)
+  1.900000   0.200000   2.100000 (  2.224832) # 2005 ThinkPad
+  0.940000   0.000000   0.940000 (  0.947986) # 2008 Eee901
   
